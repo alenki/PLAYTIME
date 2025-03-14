ea0e7235654858e2e2bcbf5db54cffb91727f6f7
Alenki <omarhack810@gmail.com>
2025-03-14 19:48:24 +0300

    small add

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby2wJQKELPDoqwEaFRSud-kl-8TArPUHs71WF9U2zZ94objwWUqPW4R7klZIgLDHQTKbA/exec'
 
 
+const contacts_button = document.getElementById('contacts_button');
+const language_button = document.getElementById('language_button');
