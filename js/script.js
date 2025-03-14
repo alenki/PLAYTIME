d3e627f9bcb0786629826371307aa7fa7153dbfb
Alenki <omarhack810@gmail.com>
2025-03-14 19:42:56 +0300

    Menu logic, main heading text adapted

--- js/script.js
+++ js/script.js
@@ -1,22 +1,3 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby2wJQKELPDoqwEaFRSud-kl-8TArPUHs71WF9U2zZ94objwWUqPW4R7klZIgLDHQTKbA/exec'
 
 
-// navbar
-const navbar = document.querySelector('.custom-navbar');
-const navbar_default = navbar.innerHTML;
-let navbar_hovered = false;
-navbar.addEventListener("transitionend", () => {
-  if(navbar_hovered){
-    navbar.innerHTML = navbar_default;
-    navbar_hovered = false;
-  }
-}); 
-navbar.addEventListener("mouseover", () => {
-  if(!navbar_hovered) {
-    navbar.innerHTML+=`
-      <div class="contacts-button ms-3 text-center" id="contacts-button"><span class="">Контакты</span></div>
-      <div class="language-button ms-3 text-center" id="language-button"><span class="">English</span></div>
-    `
-    navbar_hovered = true;
-  }
-});
