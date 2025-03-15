4431bd67cb004c99aac167984cd73db1b685173d
Alenki <omarhack810@gmail.com>
2025-03-15 20:33:08 +0300

    Added contacts modal

--- js/script.js
+++ js/script.js
@@ -1,5 +1,26 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycby2wJQKELPDoqwEaFRSud-kl-8TArPUHs71WF9U2zZ94objwWUqPW4R7klZIgLDHQTKbA/exec'
 
 
-const contacts_button = document.getElementById('contacts_button');
-const language_button = document.getElementById('language_button');
+// Home
+document.querySelector(".home-menu-button").addEventListener('click', () => {
+    location.reload();
+});
+
+// Contacts
+document.querySelector(".contacts-button").addEventListener('click', () => {
+    var modal = new bootstrap.Modal(document.getElementById('contactsModal'), {
+        keyboard: false
+    })
+    modal.toggle()
+});
+document.querySelector(".contact-main-button").addEventListener('click', () => {
+    var modal = new bootstrap.Modal(document.getElementById('contactsModal'), {
+        keyboard: false
+    })
+    modal.toggle()
+});
+
+// Language
+document.querySelector(".language-button").addEventListener('click', () => {
+    location.reload();
+});
